1d5380d64b4c5c6c56be86b731049cfd29a375bb
Brunlo <LoganCratty1@gmail.com>
2019-03-19 15:54:53 -0600

    fire!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Assignments/Lessson-10/scripts/weatherapi.js
@@ -0,0 +1,9 @@
+//current Weather
+var weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET','//api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=9ebb09608680154b476f1a983cedac5e&units=imperial', true)
+weatherRequest.send();
+
+weatherRequest.onload = function()  {
+    var weatherDaa = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+}
