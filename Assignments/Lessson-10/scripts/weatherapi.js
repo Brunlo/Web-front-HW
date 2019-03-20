421dd51e455d901f3dfe25e7dbc40eada382d684
Brunlo <LoganCratty1@gmail.com>
2019-03-20 12:05:27 -0600

    stop

--- Assignments/Lessson-10/scripts/weatherapi.js
+++ Assignments/Lessson-10/scripts/weatherapi.js
@@ -13,25 +13,25 @@ weatherRequest.onload = function()  {
 
 }
 //forcast
-var forcastObject = new XMLHttpRequest();
-forcastObject.open('GET', '//api.openweathermap.org/data/2.5/forecast?zip=5799783,us&appid=25187f2c68d8950cbad7de4d8c50ff5b&units=imperial', true)
-forcastObject.send();
+// var forcastObject = new XMLHttpRequest();
+// forcastObject.open('GET', '//api.openweathermap.org/data/2.5/forecast?zip=5799783,us&appid=25187f2c68d8950cbad7de4d8c50ff5b&units=imperial', true)
+// forcastObject.send();
 
-forcastObject.onload = function () {
-    var weatherForcast = JSON.parse(forcastObject.responseText);
-    console.log(weatherForcast);
+// forcastObject.onload = function () {
+//     var weatherForcast = JSON.parse(forcastObject.responseText);
+//     console.log(weatherForcast);
 
-    document.getElementById('day1').innerHTML = weatherForcast.list["0"].main.temp.toFixed(1);
-    document.getElementById('day2').innerHTML = weatherForcast.list["1"].main.temp.toFixed(1);
-    document.getElementById('day3').innerHTML = weatherForcast.list["2"].main.temp.toFixed(1);
-    document.getElementById('day4').innerHTML = weatherForcast.list["3"].main.temp.toFixed(1);
-    document.getElementById('day5').innerHTML = weatherForcast.list["4"].main.temp.toFixed(1);
-    document.getElementById('day6').innerHTML = weatherForcast.list["5"].main.temp.toFixed(1);
-    document.getElementById('day7').innerHTML = weatherForcast.list["6"].main.temp.toFixed(1);
-    document.getElementById('day8').innerHTML = weatherForcast.list["7"].main.temp.toFixed(1);
-    document.getElementById('day9').innerHTML = weatherForcast.list["8"].main.temp.toFixed(1);
-    document.getElementById('day10').innerHTML = weatherForcast.list["9"].main.temp.toFixed(1);
+//     document.getElementById('day1').innerHTML = weatherForcast.list["0"].main.temp.toFixed(1);
+//     document.getElementById('day2').innerHTML = weatherForcast.list["1"].main.temp.toFixed(1);
+//     document.getElementById('day3').innerHTML = weatherForcast.list["2"].main.temp.toFixed(1);
+//     document.getElementById('day4').innerHTML = weatherForcast.list["3"].main.temp.toFixed(1);
+//     document.getElementById('day5').innerHTML = weatherForcast.list["4"].main.temp.toFixed(1);
+//     document.getElementById('day6').innerHTML = weatherForcast.list["5"].main.temp.toFixed(1);
+//     document.getElementById('day7').innerHTML = weatherForcast.list["6"].main.temp.toFixed(1);
+//     document.getElementById('day8').innerHTML = weatherForcast.list["7"].main.temp.toFixed(1);
+//     document.getElementById('day9').innerHTML = weatherForcast.list["8"].main.temp.toFixed(1);
+//     document.getElementById('day10').innerHTML = weatherForcast.list["9"].main.temp.toFixed(1);
 
-}
+// }
 
-//
+// //
